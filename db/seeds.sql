0a193307603153938309b0d021a8e79640f6f5b1
Chice <cko517@hotmail.com>
2022-01-10 01:59:30 -0500

    add db/schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department(name) VALUES ('Engineering');
+INSERT INTO role(title, salary, department_id) VALUES ('Front End Dev', '1500000', '1');
+INSERT INTO employee(first_name, last_name, role_id) VALUES ('Christian', 'Ouimet', '1');
+
+INSERT INTO department(name) VALUES ('Accounting');
+INSERT INTO role(title, salary, department_id) VALUES ('Book Keeper', '500000', '2');
+INSERT INTO employee(first_name, last_name, role_id) VALUES ('test', 'testing', '2');
